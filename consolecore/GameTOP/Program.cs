c13b37ccf82a7b0263cc4b1217c4663ede6d1bba
luannascimentobjj <nascimento.hate@gmail.com>
2019-12-14 11:17:43 -0300

    inclusão total

---
+++ consolecore/GameTOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA("Pelé");
+            jogo.IniciarJogo();
+        }
+    }
+
+    class JogoFODA
+    {
+
+        private readonly string _NomeJogador;
+        public JogoFODA(string nome)
+        {
+            _NomeJogador = nome;
+        }
+
+        public void IniciarJogo()
+        {
+            Console.Write($"{_NomeJogador} deu um passe.");
+        }
+
+    }
+}
