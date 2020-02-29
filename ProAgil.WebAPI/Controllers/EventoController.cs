84fca9514307fd9e8266b09742039edd379598a6
luannascimentobjj <nascimento.hate@gmail.com>
2020-02-29 08:03:46 -0300

    criação do primeiro banco de dados com entityframework, sqlite

--- ProAgil.WebAPI/Controllers/EventoController.cs
+++ ProAgil.WebAPI/Controllers/EventoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Data;
 using ProAgil.WebAPI.Model;
 
 namespace ProAgil.WebAPI.Controllers
@@ -12,34 +13,19 @@ namespace ProAgil.WebAPI.Controllers
     public class EventoController : ControllerBase
     {
         private readonly ILogger<EventoController> _logger;
+        private readonly DataContext _context;
 
-        public EventoController(ILogger<EventoController> logger)
+        public EventoController(ILogger<EventoController> logger, DataContext context)
         {
             _logger = logger;
+            _context =  context;
+
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return new Evento[]{
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Niterói",
-                    Lote = "1º Lote",
-                    QtdPessoas = 25,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-
-                },  new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e suas novidades",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 35,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-
-                }
-            };
+          return _context.Eventos.ToList();
         }
 
         
