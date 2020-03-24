2c9b6326503aa00fbe7ee887b9663d3b8f84ae4c
luannascimentobjj <nascimento.hate@gmail.com>
2020-03-24 10:46:56 -0300

    inclusão de rotas e chamada da api com o angular

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@Angular/common/http';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
-
+import { AppRoutingModule} from './app-routing.module';
 @NgModule({
    declarations: [
       AppComponent,
       EventosComponent
    ],
    imports: [
-      BrowserModule
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
    ],
    providers: [],
    bootstrap: [
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@Angular/common/http';
 
 @Component({
   selector: 'app-eventos',
@@ -7,27 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any = [
-    {
-      EventoId: 1,
-      Tema: 'Angular',
-      Local: 'Niterói'
-    },
-    {
-      EventoId: 1,
-      Tema: '.NET Core',
-      Local: 'Niterói'
-    },
-    {
-      EventoId: 1,
-      Tema: '.NET Core e Angular',
-      Local: 'Niterói'
-    }
-  ];
+  eventos: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos();
   }
 
+  getEventos() {
+    this.http.get('http://localhost:5000/api/Evento').subscribe(response => {
+      this.eventos = response;
+  }, error => {
+    console.log(error);
+  });
+ }
 }
