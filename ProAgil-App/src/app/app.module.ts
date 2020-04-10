5b66c68a2931c6e36cb0767fdd196e34727654ec
luannascimentobjj <nascimento.hate@gmail.com>
2020-04-10 10:17:49 -0300

    incluir tooltip para os botões

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@Angular/common/http';
+import { TooltipModule, BsDropdownModule , ModalModule } from 'ngx-bootstrap';
+import { AppRoutingModule} from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
-import { AppRoutingModule} from './app-routing.module';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+
 import { DateTimeFormatPipePipe } from './_helpers/DateTimeFormatPipe.pipe';
 import { EventoService } from './_services/evento.service';
-import { TooltipModule, BsDropdownModule , ModalModule } from 'ngx-bootstrap';
+
 @NgModule({
    declarations: [
       AppComponent,
